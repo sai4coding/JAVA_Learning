c6b1e687e450eea1a2765396432720080b9eb143
sai4coding <155803032+sai4coding@users.noreply.github.com>
2025-08-16 14:25:32 +0530

    Create Pattern5.java

---
+++ Basics/Patterns/Pattern5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Pattern5
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Enter no of rows : ");
+        int row=sc.nextInt();
+        System.out.println("Enter no of columns :");
+        int col=sc.nextInt();
+        for(int i=1;i<=row;i++)
+        {
+            for(int j=1;j<=col;j++)
+            {
+                
+            
+            System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
